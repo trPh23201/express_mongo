575b76a2d3cc355166c1560fd18177c7d8c6a7e3
tripham <gnoob53@example.com>
2023-06-26 23:41:56 +0700

    restful user route

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -1,10 +1,75 @@
-const express = require('express');
-const userController = require('../../controllers/user')
+const express = require("express");
+const userController = require("../../controllers/user");
+const User = require("../../models/User");
 const router = express.Router();
 
-router.get('/', userController.getAllUser)
-router.get('/:id', userController.getUserById)
-router.delete('/:id', userController.deleteUser)
-router.put('/:id', userController.updateUser)
+router.get("/", async (req, res) => {
+  try {
+    const user = await User.find();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      throw Error("User not found");
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { name, age, phone, address, gender } = req.body;
+  try {
+    const user = await User.create({
+      name,
+      age,
+      phone,
+      address,
+      gender,
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      throw Error("User not found");
+    }
+    await user.deleteOne();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+});
+router.put("/:id", async (req, res) => {
+  const { name, age, phone, address, gender } = req.body;
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      throw Error("User not found");
+    }
+    await user.updateOne({
+      name,
+      age,
+      phone,
+      address,
+      gender,
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+});
 
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const user = require("./api/user");
 const auth = require("./api/auth");
-const checkLogin = require("../middleware/checkLogin");
-const checkManager = require("../middleware/checkManager");
+// const checkLogin = require("../middleware/checkLogin");
+// const checkManager = require("../middleware/checkManager");
 
 router.get("/", (req, res) => {
   res.json({ hello_world: "hello world" });
 });
-router.use("/user", checkLogin, checkManager, user);
+router.use("/user", user);
 router.use("/", auth);
 
 module.exports = router;
